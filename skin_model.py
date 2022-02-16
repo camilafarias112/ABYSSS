27f3131532fbdc7228ab399f25fdf865b883364c
ctanes <tanesc@e86a6429cfb6.chop.edu>
2022-02-16 11:32:31 -0500

    Bacteria can move, age and die

--- skin_model.py
+++ skin_model.py
@@ -1,15 +1,28 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
-
+#from mesa.batchrunner import batch_run
 
 class Bacteria(Agent):
-    def __init__(self, unique_id, age, model):
-        super().__init__(unique_id, age, model)
-        
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.age = 0
+    
     def move(self):
         ### bacteria can only move right with a random number of steps
-        return
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+    
+    def increment_age(self):
+        self.age += 1
+
+    def die(self):
+        if self.age > 7:
+            self.model.grid._remove_agent(self.pos, self)
+            self.model.schedule.remove(self)
 
     def generate_chemokines(self):
         return
@@ -18,15 +31,14 @@ class Bacteria(Agent):
         return
 
     def step(self):
-        return
-        ## bacteria will move
-
+        self.increment_age()
+        self.move()
         ## generate x number of Neutrophils (may have to move this to another class)
 
         ## reproduce
 
         ## die if age = x
-
+        self.die()
 
 
 #class Chemokine(Agent):
@@ -39,11 +51,10 @@ class Bacteria(Agent):
 
 
 class Neutrophil(Agent):
-    def __init__(self, unique_id, age, model):
-        super().__init__(unique_id, age, model)
-
-
-
+    def __init__(self, unique_id, model, age):
+        super().__init__(unique_id, model, age)
+        self.age = 0
+    
     def step(self):
         return
         ## neutrophils will home to a bacteria
@@ -56,8 +67,9 @@ class Neutrophil(Agent):
 class Skin(Model):
     def __init__(self, N_bacteria, width, height):
         self.num_bacteria = N_bacteria
-        self.grid = MultiGrid(width, height, True)
+        self.grid = MultiGrid(width, height, False)
         self.schedule = RandomActivation(self)
+        self.running=True
         
         # Create initial number of bacteria
         for i in range(self.num_bacteria):
@@ -67,3 +79,6 @@ class Skin(Model):
             # add the bacteria to the leftmost location with random y
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(b, (0, y))
+
+    def step(self):
+        self.schedule.step()
--- skin_model_gui.py
+++ skin_model_gui.py
@@ -8,7 +8,7 @@ def agent_portrayal(agent):
                  "Filled": "true",
                  "Layer": 0,
                  "Color": "red",
-                 "r": 0.5}
+                 "r": 0.2}
     return portrayal
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
